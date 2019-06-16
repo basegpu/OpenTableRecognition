19955c567964af3958baf67feef1188d1947a45f
Samuel Peter <sapeter@ethz.ch>
2019-06-17 01:58:40 +0200

    make test pass again

--- tests/BitmapDescriptorTests.cs
+++ tests/BitmapDescriptorTests.cs
@@ -27,8 +27,8 @@ namespace Tests
             };
             var descriptor = new BitmapDescriptor<bool>(bitmap);
             checkSize<bool>(descriptor, 3, 2);
-            checkHistogram(descriptor.RowHistogram, new double[]{1.0, 0.0, 2.0});
-            checkHistogram(descriptor.ColumnHistogram, new double[]{2.0, 1.0});
+            checkHistogram(descriptor.RowHistogram, new double[]{0.3333, 0.0, 0.6666}, 3);
+            checkHistogram(descriptor.ColumnHistogram, new double[]{0.6666, 0.3333}, 3);
         }
 
         [Fact]
@@ -36,36 +36,36 @@ namespace Tests
         {
             var bitmap = new int[2,3]{
                 {0, 1, 2},
-                {7, 8, 9}
+                {1, 4, 2}
             };
             var descriptor = new BitmapDescriptor<int>(bitmap);
             checkSize<int>(descriptor, 2, 3);
-            checkHistogram(descriptor.RowHistogram, new double[]{3, 24});
-            checkHistogram(descriptor.ColumnHistogram, new double[]{7, 9, 11});
+            checkHistogram(descriptor.RowHistogram, new double[]{0.3, 0.7});
+            checkHistogram(descriptor.ColumnHistogram, new double[]{0.1, 0.5, 0.4});
         }
 
         [Fact]
         public void FloatBitmap()
         {
             var bitmap = new float[1,5]{
-                {0.5f, 0.99f, 0.0f, 0.01f, 1.0f}
+                {0.5f, 9.0f, 0.0f, 0.01f, 0.49f}
             };
             var descriptor = new BitmapDescriptor<float>(bitmap);
             checkSize<float>(descriptor, 1, 5);
-            checkHistogram(descriptor.RowHistogram, new double[]{2.5});
-            checkHistogram(descriptor.ColumnHistogram, new double[]{0.5, 0.99, 0.0, 0.01, 1.0});
+            checkHistogram(descriptor.RowHistogram, new double[]{1.0});
+            checkHistogram(descriptor.ColumnHistogram, new double[]{0.05, 0.9, 0.0, 0.001, 0.049});
         }
 
         [Fact]
         public void DoubleBitmap()
         {
             var bitmap = new double[4,1]{
-                {0.5}, {0.99}, {1.0}, {0.01}
+                {0.9}, {8.0}, {1.0}, {0.1}
             };
             var descriptor = new BitmapDescriptor<double>(bitmap);
             checkSize<double>(descriptor, 4, 1);
-            checkHistogram(descriptor.RowHistogram, new double[]{0.5, 0.99, 1.0, 0.01});
-            checkHistogram(descriptor.ColumnHistogram, new double[]{2.5});
+            checkHistogram(descriptor.RowHistogram, new double[]{0.09, 0.8, 0.1, 0.01});
+            checkHistogram(descriptor.ColumnHistogram, new double[]{1.0});
         }
 
         private void checkSize<T>(BitmapDescriptor<T> descriptor, int rows, int columns)

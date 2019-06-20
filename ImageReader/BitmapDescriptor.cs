c40813f8e7930d101a277eb49d0163015e3fbe7e
Samuel Peter <sapeter@ethz.ch>
2019-06-20 19:31:05 +0200

    fft calculation

--- ImageReader/BitmapDescriptor.cs
+++ ImageReader/BitmapDescriptor.cs
@@ -1,8 +1,12 @@
 ﻿using SixLabors.ImageSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using Extreme.Statistics;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.IntegralTransforms;
+using MathNet.Numerics.Statistics;
 
 namespace OpenTableRecognition
 {
@@ -31,6 +35,13 @@ namespace OpenTableRecognition
             CalcHistograms();
         }
 
+        public void ApplyFiltering()
+        {
+            var ic = GetSortedFrequencies(ColumnHistogram, 0.02);
+            FilterByMovingAverage(ic[0]);
+            GetSortedFrequencies(RowHistogram, 0.02);
+        }
+
         private void CalcHistograms()
         {
             RowHistogram = new double[nRows];
@@ -56,11 +67,35 @@ namespace OpenTableRecognition
             {
                 ColumnHistogram[ci] *= invSum;
             }
-            // var rows = KernelDensity.Estimate(ColumnHistogram, KernelDensity.GaussianKernel);
-            // foreach (var item in rows)
-            // {
-            //     Console.WriteLine(item);
-            // }
+        }
+
+        private int[] GetSortedFrequencies(double[] data, double lowpass)
+        {
+            int lpf = (int)(data.Length * lowpass);
+            Complex32[] samples = new Complex32[data.Length];
+            for (int ii = 0; ii < data.Length; ii++)
+            {
+                samples[ii] = new Complex32((float)data[ii], 0.0f);
+            }
+            Fourier.Forward(samples, FourierOptions.NoScaling);
+            int Nf = data.Length/2 - lpf;
+            double[] freq = new Double[Nf];
+            for (int ii = 0; ii < Nf; ii++)
+            {
+                freq[ii] = samples[lpf+ii].Magnitude;
+            }
+            var sorted = freq
+                .Select((x, i) => new KeyValuePair<int, double>(i, x))
+                .OrderByDescending(x => x.Value)
+                .ToList();
+            freq = sorted.Select(x => x.Value).ToArray();
+            var idx = sorted.Select(x => x.Key + lpf).ToArray();
+            return idx;
+        }
+
+        private void FilterByMovingAverage(int window)
+        {
+
         }
     }
 }
